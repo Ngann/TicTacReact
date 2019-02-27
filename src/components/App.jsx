62da9d6f0a93ae69b51c6f5fd746210e2e67edbf
Student <example@example.com>
2019-02-26 17:05:46 -0800

    working code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import Header from './Header';
-import CardList from './CardList';
-import { Switch, Route } from 'react-router-dom';
-import NewCardForm from './NewCardForm';
-import NewCardControl from './NewCardControl';
-import ImageExample from './ImageExample';
-import Error404 from './Error404';
-import ExampleClassComponent from './ExampleFunctionalComponent'
+import Game from './Game';
 
 function App(){
   return (
     <div>
-      <Header/>
-      <ExampleClassComponent/>
-      <ImageExample/>
-      <Switch>
-        <Route exact path='/' component={CardList} />
-        <Route path='/newcard' component={NewCardControl} />
-        <Route component={Error404} />
-      </Switch>
+      <Game/>
     </div>
   );
 }
