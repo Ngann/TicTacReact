f56f1d9e9b07a91f56570c6beb6010a12ecde723
Student <example@example.com>
2019-02-27 14:01:58 -0800

    add constructor to Square to take state

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 class Square extends React.Component {
+  //In JavaScript classes, you need to always call super when defining the constructor of a subclass. All React component classes that have a constructor should start it with a super(props) call.
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render(){
     return (
-      <button class="sqaure">
-        {this.props.value}
+      <button className="square" onClick={ () => this.setState({value:'X'})}>
+        {this.state.value}
       </button>
     );
   }
